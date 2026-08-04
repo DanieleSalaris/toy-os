78758b3cd5f6f6fa881ea8e3d4e93e6b3369a7d6
Daniele Salaris <dnl.salaris@gmail.com>
2026-08-04 19:12:24 +0200

    Basic keyboard handling

---
+++ bool.h
@@ -0,0 +1,10 @@
+#ifndef BOOL_H
+#define BOOL_H
+#include <stdint.h>
+
+typedef enum {
+    FALSE,
+    TRUE
+} bool_t;
+
+#endif
--- keyboard.c
+++ keyboard.c
@@ -1,18 +1,33 @@
 #include <stdint.h>
+#include "bool.h"
 #include "vga.h"
-
-char keyboard_pressed_keys[88] = {
-    '*', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    '0', '-', '=', '*', '*', 'q', 'w', 'e', 'r', 't',
-    'y', 'o', 'p', '[', ']', '*', '*', 's', 'a', 'd',
-    'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', '*',
-    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.',
-    '/'
+#define CS3_PREFIX 0xE0
+#define KT_SIZE 0x40
+char keyboard_pressed_keys[KT_SIZE] = {
+    0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'',  0,  0,   0,
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 0,   '+',  0,    0,  'a', 's',
+    'd', 'f', 'g', 'h', 'j', 'k', 'l',  0,   0,  '<', 0,    0,  'z',  'x', 'c', 'v',
+    'b', 'n', 'm', ',', '.', '-',  0,   0,   0,   0,  0,    0,   0,    0,   0,   0
 };
 
+bool_t cs3_prefix_on = FALSE;
+
+
 void idt_keyboard_input_handler(uint8_t scancode) {
-    //if (scancode > 41) return;
-    // terminal_putchar(keyboard_pressed_keys[scancode - 1]);
-    terminal_put_hex(scancode);
-    terminal_putchar(' ');
+    if (scancode == CS3_PREFIX) {
+        cs3_prefix_on = TRUE;
+        return;
+    }
+    if (cs3_prefix_on && scancode == 0x5C) {
+        terminal_putchar(' ');
+    }
+    cs3_prefix_on = FALSE;
+    if (scancode >= KT_SIZE) {
+        return;
+    }
+    char c = keyboard_pressed_keys[scancode];
+    if (c != 0) {
+        terminal_putchar(c);
+    }
 }
+
